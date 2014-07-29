32b2ec995e29e9093d0169801c04a09299a6800d
BrandenOlson <BrandenOlson@github.com>
2014-07-29 15:31:48 -0600

    Removed test code

--- Problem1/problem1.rb
+++ Problem1/problem1.rb
@@ -4,14 +4,11 @@ def is_multiple_of(x, y)
    x % y == 0
 end 
 
-puts is_multiple_of(4,2)
-puts is_multiple_of(2,4)
-puts is_multiple_of(13, 1)
-
 sum = 0
 (1..999).each do |number|
     if( is_multiple_of(number, 3) || is_multiple_of(number, 5) )
       sum += number  
     end
 end
+
 puts sum
