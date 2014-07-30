af5b1575122aeb40cf874406e67715a1443872eb
BrandenOlson <BrandenOlson@github.com>
2014-07-30 14:01:45 -0600

    Solved Problem 8

--- Problem5/problem5.rb
+++ Problem5/problem5.rb
@@ -1,9 +1,10 @@
 require 'prime.rb'
 
+# Assumes that k >= 10 for the sake of the problem's context
 def is_divisible_through(n, k)
     return false if n % 2520 != 0
     (11..k).each do |candidate|
-        return false if n % candidate != 0
+        return false if 0 != n % candidate
     end
     return true      
 end
